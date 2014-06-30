5d89f08220b0b13ec5eafde208d46fb7ea69c48a
mcronald92 <ujj.tamas.i@gmail.com>
2014-06-30 19:16:08 +0200

    Modified the cluster builder script to create a manager node for Cloudera Manager

--- Cluster Administration/build_cluster.sh
+++ Cluster Administration/build_cluster.sh
@@ -2,6 +2,8 @@
 #Builds a 1master/3slave cluster from scratch
 
 echo "Creating nodes---------------------------------------"
+echo "Creating manager node"
+./create_new_node.sh manager 25 > /dev/null 2>&1 &
 echo "Creating master node"
 ./create_new_node.sh master 25 > /dev/null 2>&1 &
 echo "Creating slave node"
@@ -14,15 +16,21 @@ echo "Initializing disks (this might take a while)"
 sleep 60
 echo "Nodes are operational"
 echo
+echo "Copying installation file on manager-node------------"
+gcutil push manager-node /home/ujj/cloudera-manager-installer.bin /home/ujj/cloudera-manager-installer.bin > /dev/null 2>&1 &
+echo "Don't forget to manually install the service later"
+echo
 echo "Setting up hosts table-------------------------------"
 gcutil listinstances --columns name,network-ip | ./Util/create_hosts_file.py
 echo "Distributing hosts file"
+gcutil push manager-node hosts /home/ujj/ > /dev/null 2>&1 &
 gcutil push master-node hosts /home/ujj/ > /dev/null 2>&1 &
 gcutil push slave-a-node hosts /home/ujj/ > /dev/null 2>&1 &
 gcutil push slave-b-node hosts /home/ujj/ > /dev/null 2>&1 &
 gcutil push slave-c-node hosts /home/ujj/ > /dev/null 2>&1
 rm hosts
 sleep 5
+gcutil ssh manager-node 'sudo cp hosts /etc/hosts' > /dev/null 2>&1 &
 gcutil ssh master-node 'sudo cp hosts /etc/hosts' > /dev/null 2>&1 &
 gcutil ssh slave-a-node 'sudo cp hosts /etc/hosts' > /dev/null 2>&1 &
 gcutil ssh slave-b-node 'sudo cp hosts /etc/hosts' > /dev/null 2>&1 &
@@ -42,10 +50,11 @@ gcutil ssh slave-c-node "echo 'vm.swappiness = 0' | sudo tee -a /etc/sysctl.conf
 echo "Swapping disabled"
 echo
 echo "Rebooting"
+gcutil ssh manager-node 'sudo reboot' > /dev/null 2>&1 &
 gcutil ssh master-node 'sudo reboot' > /dev/null 2>&1 &
 gcutil ssh slave-a-node 'sudo reboot' > /dev/null 2>&1 &
 gcutil ssh slave-b-node 'sudo reboot' > /dev/null 2>&1 &
 gcutil ssh slave-c-node 'sudo reboot' > /dev/null 2>&1
 sleep 20
-echo "Cluster is ready!"
+echo "Cluster is ready! Now you should install Cloudera Manager on manager-node."
 

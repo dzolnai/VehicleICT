70eeb6b0c8a7065ac14edf97386eef74b648b3c6
mcronald92 <ujj.tamas.i@gmail.com>
2014-07-01 15:33:28 +0200

    Bug fix

--- Cluster Administration/Util/distribute_hosts_file.sh
+++ Cluster Administration/Util/distribute_hosts_file.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
+
 gcutil listinstances --columns name,network-ip | ./create_hosts_file.py
-gcutil push manager-node hosts /home/ujj/
-gcutil push master-node hosts /home/ujj/
-gcutil push slave-a-node hosts /home/ujj/
-gcutil push slave-b-node hosts /home/ujj/
-gcutil push slave-c-node hosts /home/ujj/
+gcutil push manager-node hosts ~/
+gcutil push master-node hosts ~/
+gcutil push slave-a-node hosts ~/
+gcutil push slave-b-node hosts ~/
+gcutil push slave-c-node hosts ~/
 gcutil ssh manager-node 'sudo cp hosts /etc/hosts'
 gcutil ssh master-node 'sudo cp hosts /etc/hosts'
 gcutil ssh slave-a-node 'sudo cp hosts /etc/hosts'
 gcutil ssh slave-b-node 'sudo cp hosts /etc/hosts'
 gcutil ssh slave-c-node 'sudo cp hosts /etc/hosts'
-gcutil ssh manager-node 'rm /home/ujj/hosts'
-gcutil ssh master-node 'rm /home/ujj/hosts'
-gcutil ssh slave-a-node 'rm /home/ujj/hosts'
-gcutil ssh slave-b-node 'rm /home/ujj/hosts'
-gcutil ssh slave-c-node 'rm /home/ujj/hosts'
+gcutil ssh manager-node 'rm ~/hosts'
+gcutil ssh master-node 'rm ~/hosts'
+gcutil ssh slave-a-node 'rm ~/hosts'
+gcutil ssh slave-b-node 'rm ~/hosts'
+gcutil ssh slave-c-node 'rm ~/hosts'

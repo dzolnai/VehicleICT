699fbf23db36f69a7d8751c2477b397fd3aa6177
mcronald92 <ujj.tamas.i@gmail.com>
2014-07-01 15:13:44 +0200

    Bug fix in create_slave_config_file.py

--- Cluster Administration/Util/create_slave_config_file.py
+++ Cluster Administration/Util/create_slave_config_file.py
@@ -7,7 +7,7 @@ import os
 host_pattern = re.compile('#HOST#')
 address = sys.stdin.readline()
 
-with open('config.temp') as f:
+with open('Util/config.temp') as f:
 	out = open('config.tmp', 'w')
 	for line in f:
 		out.write(re.sub(host_pattern, address, line))

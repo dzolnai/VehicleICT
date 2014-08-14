d5be4a0e271941f4b39497eb87590a260005bd44
Dániel Zolnai <zoiddani@gmail.com>
2014-08-14 20:04:07 +0200

    Correct google cloud administration scripts

--- Cluster Administration/hibernate_cluster.sh
+++ Cluster Administration/hibernate_cluster.sh
@@ -5,4 +5,6 @@ gcutil deleteinstance manager-node -f --nodelete_boot_pd > /dev/null 2>&1 &
 gcutil deleteinstance master-node -f --nodelete_boot_pd > /dev/null 2>&1 &
 gcutil deleteinstance slave-a-node -f --nodelete_boot_pd > /dev/null 2>&1 &
 gcutil deleteinstance slave-b-node -f --nodelete_boot_pd > /dev/null 2>&1 &
-gcutil deleteinstance slave-c-node -f --nodelete_boot_pd > /dev/null 2>&1
+gcutil deleteinstance slave-c-node -f --nodelete_boot_pd > /dev/null 2>&1 &
+gcutil deleteinstance slave-d-node -f --nodelete_boot_pd > /dev/null 2>&1 &
+gcutil deleteinstance slave-e-node -f --nodelete_boot_pd > /dev/null 2>&1
--- Cluster Administration/start_cluster.sh
+++ Cluster Administration/start_cluster.sh
@@ -3,10 +3,12 @@
 
 echo "Starting nodes"
 gcutil addinstance manager-node --disk=manager-node-disk,boot --machine_type=n1-standard-2 --zone=europe-west1-b > /dev/null 2>&1 &
-gcutil addinstance master-node --disk=master-node-disk,boot --machine_type=n1-highmem-2 --zone=europe-west1-b > /dev/null 2>&1 &
+gcutil addinstance master-node --disk=master-node-disk-2,boot --machine_type=n1-highmem-2 --zone=europe-west1-b > /dev/null 2>&1 &
 gcutil addinstance slave-a-node --disk=slave-a-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1 &
 gcutil addinstance slave-b-node --disk=slave-b-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1 &
-gcutil addinstance slave-c-node --disk=slave-c-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1
+gcutil addinstance slave-c-node --disk=slave-c-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1 &
+gcutil addinstance slave-d-node --disk=slave-d-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1 &
+gcutil addinstance slave-e-node --disk=slave-e-node-disk,boot --machine_type=n1-standard-1 --zone=europe-west1-b > /dev/null 2>&1
 sleep 60
 echo "Distributing hosts file"
 ./Util/distribute_hosts_file.sh
@@ -16,7 +18,9 @@ echo "Rebooting managed nodes"
 gcutil ssh master-node "sudo reboot" > /dev/null 2>&1 &
 gcutil ssh slave-a-node "sudo reboot" > /dev/null 2>&1 &
 gcutil ssh slave-b-node "sudo reboot" > /dev/null 2>&1 &
-gcutil ssh slave-c-node "sudo reboot" > /dev/null 2>&1
+gcutil ssh slave-c-node "sudo reboot" > /dev/null 2>&1 &
+gcutil ssh slave-d-node "sudo reboot" > /dev/null 2>&1 &
+gcutil ssh slave-e-node "sudo reboot" > /dev/null 2>&1
 sleep 60
 echo
 echo "Nodes started"
